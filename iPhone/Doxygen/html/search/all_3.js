18b62637dd480eb201de3dd96edd676cb51869ac
igor <dclife2009@gmail.com>
2014-04-11 18:12:16 +0300

    fixed method name (iOS)

--- iPhone/Doxygen/html/search/all_3.js
+++ iPhone/Doxygen/html/search/all_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['error',['error',['../interface_s_e_audio_stream.html#a095232a32323fc1b43568e1609cc934d',1,'SEAudioStream']]],
-  ['exprorttofile_3acompletion_3a',['exprortToFile:completion:',['../interface_s_e_audio_stream.html#ab721ccfe6c59695b85abc3fe00b37230',1,'SEAudioStream']]]
+  ['exporttofile_3acompletion_3a',['exportToFile:completion:',['../interface_s_e_audio_stream.html#aa20244d64005b2e198eb72f99d6f7bee',1,'SEAudioStream']]]
 ];
