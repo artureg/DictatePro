edf6f27da834ff8aa43d526d2588fd45032b6de6
igor <igor@localhost>
2014-04-16 21:14:42 +0300

    remove iOS Project

--- iPhone/Doxygen/html/search/properties_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['audiodescription',['audioDescription',['../interface_s_e_audio_stream.html#a96f11351b56bdd1d363ad05fb80d1a73',1,'SEAudioStream']]],
-  ['audiostream',['audioStream',['../interface_s_e_project.html#af35059bb818d498c8a96053818016a26',1,'SEProject::audioStream()'],['../interface_s_e_record.html#a91d05afe5637ae3b715c600c12a78bfb',1,'SERecord::audioStream()']]]
-];
--- iPhone/Doxygen/html/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['start',['start',['../struct_s_e_record_sound_range.html#a68e4b0862e4a25d22219776f0a9d4bc9',1,'SERecordSoundRange']]]
-];
