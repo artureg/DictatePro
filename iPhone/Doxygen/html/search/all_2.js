e673b998fb888aa2a83195d281240f8fc012046d
igor <igor@localhost>
2014-04-16 21:14:42 +0300

    remove iOS Project

--- iPhone/Doxygen/html/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['delegate',['delegate',['../interface_s_e_audio_stream_player.html#a423b5909f11bd592bbef74ac92b18a30',1,'SEAudioStreamPlayer::delegate()'],['../interface_s_e_record_audio_stream.html#a63ed0771101a0265b303e0ae90a4258a',1,'SERecordAudioStream::delegate()']]],
-  ['deleterecord_3a',['deleteRecord:',['../interface_s_e_project.html#a766d033976c34b25c714664a936431c0',1,'SEProject']]],
-  ['duration',['duration',['../interface_s_e_audio_stream.html#af5e4493693cf24fa25af34487993d693',1,'SEAudioStream::duration()'],['../struct_s_e_record_sound_range.html#a9c2cd7852875423d8bde9118fd62f85b',1,'SERecordSoundRange::duration()']]]
-];
--- iPhone/Doxygen/html/search/all_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['open',['open',['../interface_s_e_audio_stream.html#add81dd9d9bd838f3a5dc1ceae94ccf26',1,'SEAudioStream']]]
-];
