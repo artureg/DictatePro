b4777c70673b5bd7659a0f638a1fc70bd8d37668
igor <dclife2009@gmail.com>
2014-04-23 19:49:08 +0300

    add test project with framework iOS

---
+++ iPhone/SEKitTest/SEKitTest/SEKit.framework/Versions/A/Headers/SESpeexACMAudioStream.h
@@ -0,0 +1,9 @@
+//  SESpeexACMAudioStream.h
+//  SoundRecorder
+//  Created by Igor Danich igor.danich@wise-apps.com on 11.04.14.
+
+
+#import "SEAudioStream.h"
+
+@interface SESpeexACMAudioStream : SEAudioStream
+@end
