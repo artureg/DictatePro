197281f087e8d14a2999ad41a50c0dff2bdcb39f
barbara <barbara@localhost>
2014-04-23 17:15:49 +0300

    DAV-15 - FIXED

--- Android/SoundRecorder/src/com/wiseapps/davacon/SoundRecorderActivity.java
+++ Android/SoundRecorder/src/com/wiseapps/davacon/SoundRecorderActivity.java
@@ -159,7 +159,8 @@ public class SoundRecorderActivity extends Activity {
                     return;
                 }
 
-                engine.setCurrentTime(progress);
+//                engine.setCurrentTime(progress);
+                engine.setCurrentTime(DurationUtils.secondsToBytes((double) progress / 10));
             }
 
             @Override
