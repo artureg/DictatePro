c6420eebcad02e239d398bf44d1ae581d8d2f425
Igor Danich <dclife2009@gmail.com>
2014-05-13 11:39:32 +0300

    add design fixes to progress and volume sliders (iOS)

---
+++ iPhone/SoundRecorder/Classes/GUI/Views/SEProgressIndicator.h
@@ -0,0 +1,14 @@
+//
+//  SEProgressScale.h
+//  SoundRecorder
+//
+//  Created by Igor on 5/13/14.
+//  Copyright (c) 2014 Wise-Apps. All rights reserved.
+//
+
+@interface SEProgressIndicator : UIControl
+@property(nonatomic,assign) NSTimeInterval  value;
+@property(nonatomic,assign) NSTimeInterval  duration;
+@property(nonatomic,assign) NSTimeInterval  recordingDuration;
+@property(nonatomic,readonly) BOOL          isTracking;
+@end
