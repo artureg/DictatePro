a1734f9eeb6bae62d50ae70797c9e311cc906e65
Igor Danich <igor.danich@wise-apps.com>
2014-08-22 11:03:54 +0300

    add updates

--- Blackberry/Documentation/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['applicationui',['ApplicationUI',['../class_application_u_i.html',1,'']]]
-];
--- Blackberry/SoundEditor1/src/Core/SEProjectEngine.cpp
+++
@@ -1,7 +0,0 @@
-#include "SEProjectEngine.h"
-
-SEProjectEngine::SEProjectEngine(SEProject* project, QObject *parent) :
-		SEAudioStreamEngine(project->getAudioStream()) {
-	this->project = project;
-}
-
